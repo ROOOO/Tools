39c69d091ac7c4870c2c3b8abd3697cd9e77dc6f
ROOOO <ql.wang.gd@gmail.com>
2016-08-12 14:05:20 +0800

    add taskkill.

---
+++ Taskkill.py
@@ -0,0 +1,20 @@
+#coding: utf-8
+import os
+import sys
+from util import util
+
+if __name__ == '__main__':
+  u = util()
+  if u.systemFlag() != 'Windows':
+    print 'Only support Windows now.'
+    exit()
+  if len(sys.argv) <= 1:
+    print 'At least one task name.'
+    exit()
+  tasks = sys.argv
+  if sys.argv[1] == '1':
+    tasks = ['conhost*', 'powershell*']
+  for name in tasks:
+    print name
+    os.popen('start taskkill /f /im ' + name)
+
---
+++ util.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+import platform
+
+class util:
+    def __init__(self):
+        pass
+
+    def systemFlag(self):
+        return platform.system()

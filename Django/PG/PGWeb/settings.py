aa8d5ed5e1d2d1f3a50df30c8cc639c33ae93c36
ROOOO <ql.wang.gd@gmail.com>
2016-11-30 09:41:07 +0800

    remake migrations

--- Django/PG/PGWeb/settings.py
+++ Django/PG/PGWeb/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'PGWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tools',
+        'USER': 'king',
+        'PASSWORD': 'wqlwqlwql',
+        # 'HOST': '10.211.55.6',
+        'HOST': '138.68.62.86',
     }
 }
 

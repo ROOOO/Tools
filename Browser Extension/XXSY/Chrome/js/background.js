317e4a7ef0ab741890bde6991df8479cf859a20b
ROOOO <ql.wang.gd@gmail.com>
2017-02-11 17:06:53 +0800

    add chrome extension

--- Browser Extension/XXSY/Chrome/js/background.js
+++ Browser Extension/XXSY/Chrome/js/background.js
@@ -3,17 +3,51 @@ function httpRequest(url, callback){
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
-            callback(xhr.responseText);
+            var name = localStorage.xxname ? localStorage.xxname : '"blackList"';
+            var reg = new RegExp(name, "g");
+            var mat = xhr.responseText.match(reg);
+            mat = mat ? mat.length : 0;
+            callback(mat);
         }
     }
     xhr.send();
-    // setTimeout(function(){httpRequest(url, callback)}, 2000);
 }
 
-var rst_div = document.getElementById('ip_div');
-alert(localStorage.xxname)
-// setInterval(function(){
-    httpRequest('http://108.61.200.192:8080/xxsy/', function(responseText){
-        rst_div.innerText = responseText;
+function setNum() {
+    httpRequest('http://108.61.200.192/xxsy/', function(count){
+        var color = count == 0 ? '#00FF00' : '#FF0000';
+        var iconColor = count == 0 ? 'green.png' : 'red.png';
+        chrome.browserAction.setIcon({path: {'19': 'images/'+iconColor}});
+        chrome.browserAction.setIcon({path: {'38': 'images/'+iconColor}});
+        chrome.browserAction.setBadgeBackgroundColor({color: color});
+        chrome.browserAction.setBadgeText({text: count.toString()});
     });
-// }, 2000);
+    setTimeout(function(){setNum()}, 2000);
+}
+
+function notification() {
+    httpRequest('http://108.61.200.192/xxsy/', function (count) {
+        if (count != 0) {
+            chrome.notifications.create("abc", { 
+                type: "basic", 
+                iconUrl: 'images/red.png', 
+                title: 'Waring', 
+                message: '小黑账 x' + count.toString()
+                }, function() {}); 
+        }
+    });
+    var time = localStorage.xxnotitime ? localStorage.xxnotitime : 10;
+    setTimeout(function(){notification()}, time * 1000);
+}
+
+chrome.browserAction.onClicked.addListener(function(activeTab)
+{
+    var newURL = "http://108.61.200.192/xxsy/";
+    chrome.tabs.create({ url: newURL });
+});
+
+function Init() {
+    setNum()
+    notification()
+}
+Init()
--- Browser Extension/XXSY/Chrome/js/option.js
+++ Browser Extension/XXSY/Chrome/js/option.js
@@ -1,6 +1,12 @@
-var xxname = localStorage.xxname || 'all';
+var xxname = localStorage.xxname || 'blackList';
 document.getElementById('xxname').value = xxname;
-document.getElementById('save').onclick = function(){
+document.getElementById('save1').onclick = function(){
     localStorage.xxname = document.getElementById('xxname').value;
+    alert('保存成功。');
+}
+var xxnotitime = localStorage.xxnotitime || 10;
+document.getElementById('xxnotitime').value = xxnotitime;
+document.getElementById('save2').onclick = function(){
+    localStorage.xxnotitime = document.getElementById('xxnotitime').value;
     alert('保存成功。');
 }
